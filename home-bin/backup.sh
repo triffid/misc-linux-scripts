fdb9a07814acef2173f511172194e81d3f2d26b5
Michael Moon <triffid.hunter@gmail.com>
2019-06-04 16:59:52 +0800

    backup: ensure we have permission to delete stale file trees

--- home-bin/backup.sh
+++ home-bin/backup.sh
@@ -9,7 +9,7 @@ export PATH="$HOME/bin:/bin:/usr/bin:/usr/local/bin"
 
 LINKDEST=$(echo "$PREFIX" | perl -pe 's![^/]+!..!g;'; echo /../; )
 
-ssh "$REMOTEHOST" 'for I in {13..0}; do while [ -e "'$PREFIX'"$(( $I + 1 )) ]; do echo rm -rf '"$PREFIX"'$(( $I + 1 )) && rm -rf '"$PREFIX"'$(( $I + 1 )); done; echo mv '"$PREFIX"'$I '"$PREFIX"'$(( $I + 1 )) && mv '"$PREFIX"'$I '"$PREFIX"'$(( $I + 1 )); done; echo mkdir '"$PREFIX"'0 && mkdir '"$PREFIX"'0;'
+ssh "$REMOTEHOST" 'for I in {13..0}; do while [ -e "'$PREFIX'"$(( $I + 1 )) ]; do echo rm -rf '"$PREFIX"'$(( $I + 1 )) && chmod u+w -R '"$PREFIX"'$(( $I + 1 )) && rm -rf '"$PREFIX"'$(( $I + 1 )) || exit 1; done; echo mv '"$PREFIX"'$I '"$PREFIX"'$(( $I + 1 )) && mv '"$PREFIX"'$I '"$PREFIX"'$(( $I + 1 )) || exit 1; done; echo mkdir '"$PREFIX"'0 && mkdir '"$PREFIX"'0;'
 
 cd ~/.backup
 for F in *
