daf9699ae0e984ac4d64deb2aaf06efb443179e2
Michael Moon <triffid.hunter@gmail.com>
2019-08-08 13:54:18 +0800

    backup: bit of streamlining to hopefully speed things up

--- home-bin/backup.sh
+++ home-bin/backup.sh
@@ -9,14 +9,14 @@ export PATH="$HOME/bin:/bin:/usr/bin:/usr/local/bin"
 
 LINKDEST=$(echo "$PREFIX" | perl -pe 's![^/]+!..!g;'; echo /../; )
 
-ssh "$REMOTEHOST" 'for I in {13..0}; do while [ -e "'$PREFIX'"$(( $I + 1 )) ]; do echo rm -rf '"$PREFIX"'$(( $I + 1 )) && chmod u+w -R '"$PREFIX"'$(( $I + 1 )) && rm -rf '"$PREFIX"'$(( $I + 1 )) || exit 1; done; echo mv '"$PREFIX"'$I '"$PREFIX"'$(( $I + 1 )) && mv '"$PREFIX"'$I '"$PREFIX"'$(( $I + 1 )) || exit 1; done; echo mkdir '"$PREFIX"'0 && mkdir '"$PREFIX"'0;'
+ssh "$REMOTEHOST" 'if [ -e .old ]; then rm -rf .old &>/dev/null || exit 1; fi; if [ -e "'$PREFIX'"14 ]; then mv '"$PREFIX"'14 .old && setsid rm -rf .old &>/dev/null & || exit 1; fi; for I in {13..0}; do J=$(( $I + 1 )); echo mv '"$PREFIX"'$I '"$PREFIX"'$J && mv '"$PREFIX"'$I '"$PREFIX"'$J || exit 1; done; echo mkdir '"$PREFIX"'0 && mkdir '"$PREFIX"'0;' || exit 1
 
 cd ~/.backup
 for F in *
 do
     termtitle "Rsyncing $F"
     echo "*** $F ***"
-    rsync -avhszP --delete --link-dest="../../${PREFIX##*/}1/$F" "$F/" "${REMOTEHOST}:${PREFIX}0/$F"
+    rsync -avhszP --delete --link-dest="../../${PREFIX##*/}1/$F" "$F/" "${REMOTEHOST}:${PREFIX}0/$F" || exit 1
 done
 
 ssh "$REMOTEHOST" "touch ${PREFIX}0"
