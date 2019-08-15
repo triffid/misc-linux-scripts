916f9510a677ace1e5d73d4e7981dc91f9c0fbe0
Michael Moon <triffid.hunter@gmail.com>
2019-08-15 16:42:52 +0800

    backup.sh: fix some typos, remove redundant LINKDEST

--- home-bin/backup.sh
+++ home-bin/backup.sh
@@ -7,9 +7,7 @@ export PATH="$HOME/bin:/bin:/usr/bin:/usr/local/bin"
 
 . "$HOME/.ssh/agent"
 
-LINKDEST=$(echo "$PREFIX" | perl -pe 's![^/]+!..!g;'; echo /../; )
-
-ssh "$REMOTEHOST" 'if [ -e .old ]; then rm -rf .old &>/dev/null || exit 1; fi; if [ -e "'$PREFIX'"14 ]; then mv '"$PREFIX"'14 .old && setsid rm -rf .old &>/dev/null & || exit 1; fi; for I in {13..0}; do J=$(( $I + 1 )); echo mv '"$PREFIX"'$I '"$PREFIX"'$J && mv '"$PREFIX"'$I '"$PREFIX"'$J || exit 1; done; echo mkdir '"$PREFIX"'0 && mkdir '"$PREFIX"'0;' || exit 1
+ssh "$REMOTEHOST" 'if [ -e .old ]; then rm -rf .old &>/dev/null || exit 1; fi; if [ -e "'$PREFIX'"14 ]; then mv '"$PREFIX"'14 .old && setsid rm -rf .old &>/dev/null & echo -n; fi; for I in {13..0}; do J=$(( $I + 1 )); echo mv '"$PREFIX"'$I '"$PREFIX"'$J && mv '"$PREFIX"'$I '"$PREFIX"'$J || exit 1; done; echo mkdir '"$PREFIX"'0 && mkdir '"$PREFIX"'0;' || exit 1
 
 cd ~/.backup
 for F in *
