79b6fa38f536c1edfaeca186279e52616ca8499c
Michael Moon <triffid.hunter@gmail.com>
2019-09-27 16:51:28 +0800

    Make a little more robust against cancelled/failed transfers, print a little more debug info

--- home-bin/backup.sh
+++ home-bin/backup.sh
@@ -7,13 +7,15 @@ export PATH="$HOME/bin:/bin:/usr/bin:/usr/local/bin"
 
 . "$HOME/.ssh/agent"
 
-ssh "$REMOTEHOST" 'if [ -e .old ]; then rm -rf .old &>/dev/null || exit 1; fi; if [ -e "'$PREFIX'"14 ]; then mv '"$PREFIX"'14 .old && setsid rm -rf .old &>/dev/null & echo -n; fi; for I in {13..0}; do J=$(( $I + 1 )); echo mv '"$PREFIX"'$I '"$PREFIX"'$J && mv '"$PREFIX"'$I '"$PREFIX"'$J || exit 1; done; echo mkdir '"$PREFIX"'0 && mkdir '"$PREFIX"'0;' || exit 1
+ssh "$REMOTEHOST" 'if [ -e .old ]; then echo rm -rf .old && chmod -R u+w .old && rm -rf .old || exit 1; fi; if [ -e "'$PREFIX'"14 ]; then mv '"$PREFIX"'14 .old && setsid rm -rf .old &>/dev/null & echo -n; fi; for I in {13..0}; do J=$(( $I + 1 )); echo mv '"$PREFIX"'$I '"$PREFIX"'$J && mv '"$PREFIX"'$I '"$PREFIX"'$J || exit 1; done; echo mkdir '"$PREFIX"'0 && mkdir '"$PREFIX"'0;' || exit 1
+
+echo "Finished preparing"
 
 cd ~/.backup
 for F in *
 do
     termtitle "Rsyncing $F"
-    echo "*** $F ***"
+    echo "*** $F *** rsync -avhszP --delete --link-dest="../../${PREFIX##*/}1/$F" "$F/" "${REMOTEHOST}:${PREFIX}0/$F""
     rsync -avhszP --delete --link-dest="../../${PREFIX##*/}1/$F" "$F/" "${REMOTEHOST}:${PREFIX}0/$F" || exit 1
 done
 
